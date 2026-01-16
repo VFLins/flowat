2287a936ec9855dde72627994b1f33b0d3d04c53
Vitor <vflins@live.com>
2026-01-16 10:39:21 -0300

    update buttons

--- src/flowat/const/style.py
+++ src/flowat/const/style.py
@@ -1,3 +1,5 @@
 from toga.style import Pack
 
-BIG_BUTTON = Pack(width=240, margin=5)
+
+BIG_BUTTON = Pack(width=220, font_size=13, margin=5)
+BIG_SQUARE_BUTTON = Pack(width=50, height=50, margin=5)
--- src/flowat/pages/main.py
+++ src/flowat/pages/main.py
@@ -1,6 +1,7 @@
+from toga.widgets.label import Label
 from toga.widgets.button import Button
 from toga.widgets.divider import Divider
-from toga.widgets.box import Box, Column
+from toga.widgets.box import Box, Column, Row
 from toga.widgets.optioncontainer import OptionContainer
 from toga.style import Pack
 
@@ -10,23 +11,26 @@ from flowat.const import style, icon
 class MainSection(BaseSection):
     def __init__(self):
         super().__init__()
-        self.add_expense_button = Button(icon=icon.CIRCLE_MINUS, style=style.BIG_BUTTON)
-        self.add_revenue_button = Button(icon=icon.CIRCLE_PLUS, style=style.BIG_BUTTON)
-        self.fetch_revenue_button = Button(icon=icon.SCAN_SEARCH, style=style.BIG_BUTTON)
+        self.add_expense_button = Button(
+            "Inserir uma despesa", style=style.BIG_BUTTON,
+        )
+        self.add_revenue_button = Button(
+            "Inserir uma receita", style=style.BIG_BUTTON,
+        )
+        self.fetch_revenue_button = Button(
+            "Procurar transações", style=style.BIG_BUTTON,
+        )
 
         self.buttons_container = Column(
             style=Pack(align_items="center", flex=1),
             children=[
                 self.add_expense_button,
                 self.add_revenue_button,
+                Divider(width=180, margin=10),
                 self.fetch_revenue_button,
             ],
         )
-        self.main_container = Box(
+        self.full_contents = Box(
             style=Pack(align_items="center", flex=1),
             children=[self.buttons_container],
-        )
-        self.full_contents = OptionContainer(
-            style=Pack(align_items="center"),
-            content=[("Home", self.main_container)]
         )
