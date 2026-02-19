5b985e09da765a0c723e265d48bfc4b1d821f36e
Vitor F. Lins <vflins@live.com>
2026-02-19 11:41:34 -0300

    add: read_nflogic_revenues logic

--- src/flowat/data/nf.py
+++ src/flowat/data/nf.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Engine, inspect, select
+from sqlalchemy import create_engine, Engine, inspect, select, Table, MetaData
 from sqlalchemy.orm import Session
 from typing import Generator
 import pandas as pd
@@ -15,7 +15,7 @@ class TableName:
 
     @property
     def table_name(self) -> str:
-        return self._tablename
+        return self._table_name
 
     @property
     def display_name(self) -> str:
@@ -27,12 +27,18 @@ class TableName:
         )
 
 
+def _get_table_by_name(table_name: str, engine: Engine) -> Table:
+    """Returns a `sqlalchemy.Table` object from name and engine."""
+    meta = MetaData()
+    return Table(name=table_name, metadata=meta, autoload_with=engine)
+
+
 def read_flowat_revenues(engine: Engine = db.DB_ENGINE) -> pd.DataFrame:
     """Reads data from flowat's database to get all documents already scanned."""
     with Session(engine) as ses:
         stmt = (
             select(
-                db.RevenueEntry.Id,
+                db.ScannedInvoiceFile.Id,
                 db.ScannedInvoiceFile.DocumentIdentifier,
                 db.RevenueEntry.TimeStamp,
                 db.RevenueEntry.Description,
@@ -41,19 +47,28 @@ def read_flowat_revenues(engine: Engine = db.DB_ENGINE) -> pd.DataFrame:
             )
             .where(db.RevenueEntry.Id == db.ScannedInvoiceFile.IdRevenueEntry)
             .join(
-                db.RevenueEntry,
+                db.ScannedInvoiceFile,
                 db.RevenueEntry.Id == db.ScannedInvoiceFile.IdRevenueEntry,
             )
         )
-        print(stmt)
         res = ses.execute(stmt)
         return pd.DataFrame(data=res, columns=[c.name for c in stmt.selected_columns])
 
 
 def read_nflogic_revenues(
-    seller_name: TableName, engine: Engine = db.DB_ENGINE
+    seller_name: TableName, engine: Engine = NFLOGIC_ENGINE
 ) -> pd.DataFrame:
     """Reads data from nflogic's database to get all documents already scanned."""
+    table = _get_table_by_name(table_name=seller_name.table_name, engine=engine)
+    with Session(engine) as ses:
+        stmt = select(
+            table.c.Id,
+            table.c.ChaveNFe,
+            table.c.DataHoraEmi,
+            table.c.TotalProdutos,
+        )
+        res = ses.execute(stmt)
+        return pd.DataFrame(data=res, columns=[c.name for c in stmt.selected_columns])
 
 
 def get_seller_names(engine: Engine = NFLOGIC_ENGINE) -> list[TableName]:
