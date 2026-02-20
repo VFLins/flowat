c14648e97fd50d599693fa70c956cd672fd9c315
Vitor F. Lins <vflins@live.com>
2026-02-20 12:49:40 -0300

    upd: refactor get_new_seller_data to use minimal python runtime

--- src/flowat/data/nf.py
+++ src/flowat/data/nf.py
@@ -1,4 +1,15 @@
-from sqlalchemy import create_engine, Engine, inspect, select, Table, MetaData
+from sqlalchemy import (
+    create_engine,
+    Engine,
+    inspect,
+    select,
+    not_,
+    Table,
+    MetaData,
+    text,
+    Column,
+    String,
+)
 from sqlalchemy.orm import Session
 from typing import Generator
 import pandas as pd
@@ -33,6 +44,16 @@ def _get_table_by_name(table_name: str, engine: Engine) -> Table:
     return Table(name=table_name, metadata=meta, autoload_with=engine)
 
 
+def _get_registered_document_identifiers(
+    engine: Engine = db.DB_ENGINE,
+) -> Generator[str, None, None]:
+    """Returns a list of document identifiers registered in Flowat's database."""
+    with Session(engine) as ses:
+        stmt = select(db.ScannedInvoiceFile.DocumentIdentifier)
+        res = ses.execute(stmt)
+        return (r.DocumentIdentifier for r in res)
+
+
 def read_flowat_revenues(engine: Engine = db.DB_ENGINE) -> pd.DataFrame:
     """Reads data from flowat's database to get all documents already scanned."""
     with Session(engine) as ses:
@@ -55,20 +76,41 @@ def read_flowat_revenues(engine: Engine = db.DB_ENGINE) -> pd.DataFrame:
         return pd.DataFrame(data=res, columns=[c.name for c in stmt.selected_columns])
 
 
-def read_nflogic_revenues(
-    seller_name: TableName, engine: Engine = NFLOGIC_ENGINE
-) -> pd.DataFrame:
-    """Reads data from nflogic's database to get all documents already scanned."""
-    table = _get_table_by_name(table_name=seller_name.table_name, engine=engine)
-    with Session(engine) as ses:
-        stmt = select(
-            table.c.Id,
-            table.c.ChaveNFe,
-            table.c.DataHoraEmi,
-            table.c.TotalProdutos,
-        )
-        res = ses.execute(stmt)
-        return pd.DataFrame(data=res, columns=[c.name for c in stmt.selected_columns])
+def get_new_seller_data(
+    seller_name: TableName,
+    internal_engine: Engine = db.DB_ENGINE,
+    nf_engine: Engine = NFLOGIC_ENGINE,
+) -> Generator[tuple, None, None]:
+    """Get a list of rows collected from nflogic's database that is not present in a
+    flowat table.
+    """
+    registered_docs = _get_registered_document_identifiers(engine=internal_engine)
+    NF_TABLE = _get_table_by_name(table_name=seller_name.table_name, engine=nf_engine)
+    # NOTE: use temporary table INSERT to avoid using a SELECT statement with a large
+    # NOT IN clause
+    TMP_TABLE = Table(
+        "FLOWAT_TEMPORARY",
+        NF_TABLE.metadata,
+        Column("DocId", String, primary_key=True),
+        prefixes=["TEMPORARY"],
+    )
+    with Session(bind=nf_engine) as ses:
+        try:
+            TMP_TABLE.create(bind=nf_engine, checkfirst=True)
+            ses.execute(insert(TMP_TABLE), registered_docs)
+            stmt = (
+                select(
+                    NF_TABLE.c.Id,
+                    TMP_TABLE.c.DocId,
+                    NF_TABLE.c.DataHoraEmi,
+                    NF_TABLE.c.TotalProdutos,
+                ).
+                join(TMP_TABLE, NF_TABLE.c.ChaveNFe == TMP_TABLE.c.DocId)
+            )
+            res = ses.execute(stmt)
+            return res
+        finally:
+            TMP_TABLE.drop(bind=nf_engine, checkfirst=True)
 
 
 def get_seller_names(engine: Engine = NFLOGIC_ENGINE) -> list[TableName]:
@@ -78,19 +120,4 @@ def get_seller_names(engine: Engine = NFLOGIC_ENGINE) -> list[TableName]:
         TableName(table_name=name)
         for name in insp.get_table_names()
         if name[:6] == "VENDA_"
-    ]
-
-
-def get_new_seller_data(
-    seller_name: TableName,
-    internal_engine: Engine = db.DB_ENGINE,
-    nf_engine: Engine = NFLOGIC_ENGINE,
-) -> Generator[tuple, None, None]:
-    """Get a list of rows collected from nflogic's database that is not present in a
-    flowat table.
-    """
-    nflogic_df = read_nflogic_revenues(seller_name=seller_name)
-    flowat_df = read_flowat_revenues()
-    new_data = nflogic_df.loc[
-        ~nflogic_df["ChaveNFe"].is_in(flowat_df["DocumentIdentifier"])
     ]
