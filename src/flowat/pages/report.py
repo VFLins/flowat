fcb89b1efc57d6f7ce1bce6fcfc611d64c2200ad
Vitor F. Lins <vflins@live.com>
2026-04-18 07:23:06 -0300

    add: update plot controls

--- src/flowat/pages/report.py
+++ src/flowat/pages/report.py
@@ -78,7 +78,8 @@ def report_screen(data_source: source._DataSource | None, page: BaseSection) -> 
          print("canvas content, wow")
 
     def data_widget(type: Literal["plot", "table"]) -> WebView | Table:
-        s = Pack(flex=1, width=style.FORM_WIDTH, height=int(style.FORM_WIDTH * 0.6))
+        width = style.FORM_WIDTH if current_platform == "linux" else int(style.FORM_WIDTH * 1.4)
+        s = Pack(flex=1, width=width, height=int(width * 0.6))
         match type:
             case "plot":
                 plot = colplot(x=[], y=[])
@@ -86,19 +87,31 @@ def report_screen(data_source: source._DataSource | None, page: BaseSection) -> 
             case _:
                 return Table(style=s, headings=["Data", "Exemplo"])
 
-    return_button = Button("Voltar", on_press=lambda w: page.show_main_content())
-    print_button = Button("Imprimir", on_press=print_canvas_content)
+    return_button = Button("Voltar", style=style.RIGHTMOST_SIMPLE_BUTTON, on_press=lambda w: page.show_main_content())
+    print_button = Button("Imprimir", style=style.RIGHTMOST_SIMPLE_BUTTON, on_press=print_canvas_content)
     select_view = FormField(
         label="Tipo",
         input_widget=Selection(items=["Tabela", "Gráfico"], on_change=change_view)
     )
+    sample_size = FormField(
+        label="Qtd. de meses",
+        input_widget=NumberInput(value=6, min=1, max=52, step=1),
+    )
+    sample_horizon = FormField(
+        label="Tempo",
+        input_widget=Selection(items=["Passado", "Passado+Futuro", "Futuro"])
+    )
+
+    def change_freq(widget: Selection):
+        match widget.value:
+            case "Mensal":
+                sample_size.label.text = "Qtd. de meses"
+            case "Semanal":
+                sample_size.label.text = "Qtd. de semanas"
+
     select_freq = FormField(
         label="Frequência",
-        input_widget=Selection(style=Pack(margin=(0, 5)), items=["Mensal", "Semanal"])
-    )
-    sample_size = FormField(
-        label="Quantidade",
-        input_widget=NumberInput(value=6, min=1, max=52, step=1),
+        input_widget=Selection(style=Pack(margin=(0, 5)), items=["Mensal", "Semanal"], on_change=change_freq)
     )
     header = Row(
         style=Pack(margin_bottom=10, align_items="end"),
@@ -107,7 +120,7 @@ def report_screen(data_source: source._DataSource | None, page: BaseSection) -> 
     canvas = Column(style=Pack(align_items="center"), children=[data_widget("table")])
     footer = Row(
         style=Pack(margin_bottom=20, align_items="end"),
-        children=[sample_size, Box(style=Pack(flex=1)), return_button]
+        children=[sample_horizon, sample_size, Box(style=Pack(flex=1)), return_button]
     )
     return Column(
         style=Pack(width=style.CONTENT_WIDTH, align_items="center"),
